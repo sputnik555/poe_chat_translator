ba2de1dfec6c56d0206723afae1281b282edad0e
Alexey Beletsky <sputnik2009@gmail.com>
2023-04-19 17:57:47 +0300

    skip auto-messages

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from googletrans import Translator
 
 translator = Translator()
 
+
 if __name__ == '__main__':
-    line_count = 0
+    line_count = -1
     while True:
         lines = []
-        with open("Client.txt", 'r', encoding="utf-8") as file:
+        i = 0
+        with open('D:\Games\PoE\logs\Client.txt', 'r', encoding='utf-8') as file:
             for i, x in enumerate(file):
-                if line_count and i > line_count:
+                if i > line_count > -1:
                     lines.append(x)
         line_count = i
         for line in lines:
             message_start_index = line.find('@From')
-            if message_start_index > -1:
+            is_message_from_trade = "Hi, I'd like to buy your" in line \
+                                    or "Hi, I would like to buy your" in line
+            if message_start_index > -1 and not is_message_from_trade:
                 message_with_nickname = line[message_start_index+6:]
                 nickname = message_with_nickname.split(':')[0]
                 message_text_en = ":".join(message_with_nickname.split(':')[1:])

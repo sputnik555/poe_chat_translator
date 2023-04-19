e692b7dd7febec0797af70d394c399e2b93902f5
Alexey Beletsky <sputnik2009@gmail.com>
2023-04-19 15:28:40 +0300

    rename variables

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ if __name__ == '__main__':
         lines = []
         with open("Client.txt", 'r', encoding="utf-8") as file:
             for i, x in enumerate(file):
-                if i > line_count and line_count:
+                if line_count and i > line_count:
                     lines.append(x)
         line_count = i
         for line in lines:
             message_start_index = line.find('@From')
             if message_start_index > -1:
-                full_string_en = line[message_start_index+6:]
-                nickname = full_string_en.split(':')[0]
-                message_en = ":".join(full_string_en.split(':')[1:])
-                translated_text = translator.translate(message_en, dest='ru')
+                message_with_nickname = line[message_start_index+6:]
+                nickname = message_with_nickname.split(':')[0]
+                message_text_en = ":".join(message_with_nickname.split(':')[1:])
+                translated_text = translator.translate(message_text_en, dest='ru')
                 print("{}: {}".format(nickname, translated_text.text))
         time.sleep(5)
